10290eb1221bf5496c5de63d7c67027ba805a462
benoitantelme <benoitantelme@gmail.com>
2020-04-30 16:30:56 +0100

    First connection code

--- src/main/java/connect/App.java
+++ src/main/java/connect/App.java
@@ -1,13 +1,25 @@
 package connect;
 
+import connect.storage.RedisConnector;
+
 /**
- * Hello world!
+ *
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        String ip = "192.170.20.46";
+        RedisConnector connector = new RedisConnector();
+
+        for(int i = 0; i < 20; i++)
+            connector.incrementIp(ip);
+
+        System.out.println("IP " + ip + " seen " + connector.getIpOccurence(ip) + " times");
+
+        connector.close();
+
+        System.out.println( "Done" );
     }
 }
---
+++ src/main/java/connect/storage/RedisConnector.java
@@ -0,0 +1,73 @@
+package connect.storage;
+
+import redis.clients.jedis.*;
+
+import java.time.Duration;
+
+public class RedisConnector {
+    private static final String PREFIX = "connections/ip/";
+
+    private JedisPool jedisPool = new JedisPool(buildPoolConfig(), "localhost");
+
+    public boolean incrementIp(String ip){
+        Jedis jedis ;
+        boolean success;
+        try {
+            jedis = jedisPool.getResource();
+            if (jedis == null)
+                success = false;
+            else {
+                String key = PREFIX + ip;
+                Long result = jedis.incrBy(key, 1l);
+                System.out.println(key + " updated to " + result);
+                success = true;
+            }
+        } catch (Exception e) {
+            success = false;
+        }
+
+        return success;
+    }
+
+    public Long getIpOccurence(String ip){
+        Jedis jedis ;
+        Long result = null;
+
+        try {
+            jedis = jedisPool.getResource();
+            if (jedis != null){
+                String key = PREFIX + ip;
+                String res = jedis.get(key);
+                System.out.println(key + " updated to " + res);
+                result = Long.valueOf(res);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+
+
+    public void close(){
+        jedisPool.close();
+    }
+
+    private JedisPoolConfig buildPoolConfig() {
+        final JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+        poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+        return poolConfig;
+    }
+
+
+}
