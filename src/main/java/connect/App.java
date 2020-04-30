e052277fc444ef45d62dbda816fd6a275abeabda
benoitantelme <benoitantelme@gmail.com>
2020-04-30 13:39:30 +0100

    Project skeleton

---
+++ src/main/java/connect/App.java
@@ -0,0 +1,13 @@
+package connect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
