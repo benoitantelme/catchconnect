0ff6f61ba097928fb1d3bd8efabe998602556126
benoitantelme <benoitantelme@gmail.com>
2020-05-03 12:40:10 +0100

    Starting test for workflow

--- src/test/java/org/catchconnect/AppTest.java
+++ src/test/java/org/catchconnect/AppTest.java
@@ -2,19 +2,45 @@ package org.catchconnect;
 
 import static org.junit.Assert.assertTrue;
 
+import org.catchconnect.source.ConnectionGenerator;
+import org.catchconnect.storage.RedisConnector;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import redis.embedded.RedisServer;
 
 /**
- * Unit test for simple App.
+ * Unit test for workflow
  */
-public class AppTest 
-{
-    /**
-     * Will contain main test
-     */
+public class AppTest {
+    private final int port = 6379;
+    RedisServer redisServer;
+    RedisConnector connector;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void worfklowTest() {
+        ConnectionGenerator generator = new ConnectionGenerator(connector);
+
+
+        generator.generateIps(100);
+
+        assertTrue(connector.getTopK(10).size() == 10);
+
+
+        assertTrue(true);
+    }
+
+    @Before
+    public void setUp() {
+        redisServer = new RedisServer(port);
+        redisServer.start();
+
+        connector = new RedisConnector(port);
+    }
+
+    @After
+    public void tearDown() {
+        connector.close();
+        redisServer.stop();
     }
 }
